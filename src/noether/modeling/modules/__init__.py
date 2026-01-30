3ba2e9534a4ddaa4588aa2f8b4eea11fa2618277
Maurits Bleeker <maurits@emmi.ai>
2026-01-30 16:03:44 +0000

    fix tests that failed due to removed denormalize function

--- src/noether/modeling/modules/__init__.py
+++ src/noether/modeling/modules/__init__.py
@@ -3,7 +3,7 @@
 from .attention import DotProductAttention, PerceiverAttention, TransolverAttention
 from .blocks import PerceiverBlock, TransformerBlock
 from .decoders import DeepPerceiverDecoder
-from .encoders import MlpEncoder, SupernodePooling
+from .encoders import SupernodePooling
 from .layers import ContinuousSincosEmbed, LayerScale, LinearProjection, UnquantizedDropPath
 from .mlp import MLP, UpActDownMlp
 
