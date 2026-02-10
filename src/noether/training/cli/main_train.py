c9d4908a38087cff105826a3020725c3bbf9c190
Maurits Bleeker <44800747+MauritsBleeker@users.noreply.github.com>
2026-02-10 13:16:51 +0100

    Update src/noether/training/cli/main_train.py
    Co-authored-by: Markus Hennerbichler <markus@emmi.ai>
    Signed-off-by: Maurits Bleeker <44800747+MauritsBleeker@users.noreply.github.com>

--- src/noether/training/cli/main_train.py
+++ src/noether/training/cli/main_train.py
@@ -26,7 +26,8 @@ def main(config: DictConfig):
 
     Example:
     .. code-block:: bash
-        python main_train.py configs/my_experiment.yaml
+
+       python main_train.py configs/my_experiment.yaml
         python main_train.py --hp configs/my_experiment.yaml
         python main_train.py configs/my_experiment.yaml trainer.max_epochs=10
     """
