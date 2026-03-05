469ed758e9fdf4b1643565a9c8bf6e1f0f85e900
Maurits Bleeker <maurits@emmi.ai>
2026-03-05 15:46:39 +0000

    refactor: simplify the model by creating the transformer block config with pydantic and not inside the model itself

--- src/noether/core/schemas/models/transformer.py
+++ src/noether/core/schemas/models/transformer.py
@@ -17,6 +17,9 @@ class TransformerConfig(TransformerBlockConfig, ModelBaseConfig):
     mlp_expansion_factor: int = Field(4, ge=1)
     """Expansion factor for the MLP hidden dimension relative to the hidden dimension. If 'mlp_hidden_dim' is not set, this factor is used to compute it as hidden_dim * mlp_expansion_factor."""
 
+    transformer_block_config: TransformerBlockConfig | None = None
+    """Optional pre-defined configuration for the transformer blocks. If not provided, it will be automatically created from the parent config parameters."""
+
     @model_validator(mode="after")
     def set_mlp_hidden_dim(self):
         # Validate hidden_dim is divisible by num_heads
@@ -27,4 +30,13 @@ class TransformerConfig(TransformerBlockConfig, ModelBaseConfig):
             if self.mlp_expansion_factor is None:
                 raise ValueError("Either 'mlp_hidden_dim' or 'mlp_expansion_factor' must be provided.")
             self.mlp_hidden_dim = self.hidden_dim * self.mlp_expansion_factor
+
+        return self
+
+    @model_validator(mode="after")
+    def create_transformer_block_config(self):
+        if self.transformer_block_config is not None:
+            return self
+        block_fields = TransformerBlockConfig.model_fields.keys()
+        self.transformer_block_config = TransformerBlockConfig(**{k: getattr(self, k) for k in block_fields})
         return self
