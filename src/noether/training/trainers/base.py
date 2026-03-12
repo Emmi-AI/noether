5168b2bd20ee471ad8dd44cf906d5b8628302d5b
Maurits Bleeker <maurits@emmi.ai>
2026-03-12 11:51:47 +0000

    chore: minor code optimizations

--- src/noether/training/trainers/base.py
+++ src/noether/training/trainers/base.py
@@ -449,6 +449,7 @@ class BaseTrainer:
             samples=self.end_checkpoint.sample,
             callback_samplers=configs,
             evaluation=evaluation,
+            prefetch_factor=self.config.dataloader_prefetch_factor,
             **kwargs,
         )
 
