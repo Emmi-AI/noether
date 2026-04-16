f07fc99bc9f7b9cbb0b7d045c701fbd72198d4b0
Maurits Bleeker <maurits@emmi.ai>
2026-04-16 11:36:53 +0000

    change wrong schema validation method name

--- src/noether/core/schemas/models/transolver.py
+++ src/noether/core/schemas/models/transolver.py
@@ -16,8 +16,8 @@ class TransolverConfig(TransformerConfig, ModelBaseConfig):
     attention_arguments: dict = {"num_slices": 512}  # test if this can be overwritten in the model config
 
     @model_validator(mode="after")
-    def set_condition_dim(self) -> "TransolverConfig":
-        """Set condition_dim in transformer_block_config based on data_specs."""
+    def set_attention_constructor(self) -> "TransolverConfig":
+        """Set attention_constructor in transformer_block_config based on data_specs."""
         self.transformer_block_config.attention_constructor = "transolver"  # type: ignore[assignment]
 
         return self
@@ -27,8 +27,8 @@ class TransolverPlusPlusConfig(TransolverConfig):
     """Configuration for a Transolver++ model."""
 
     @model_validator(mode="after")
-    def set_condition_dim(self) -> "TransolverPlusPlusConfig":
-        """Set condition_dim in transformer_block_config based on data_specs."""
+    def set_attention_constructor(self) -> "TransolverPlusPlusConfig":
+        """Set attention_constructor in transformer_block_config based on data_specs."""
         self.transformer_block_config.attention_constructor = "transolver_plusplus"  # type: ignore[assignment]
 
         return self
